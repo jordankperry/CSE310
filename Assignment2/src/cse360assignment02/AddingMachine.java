eb12331cf10425e3411362f2fa13dda785360b95
Jordan <Jordan@jLaptop>
2020-09-30 17:47:52 -0700

    Added functionality to code.

--- Assignment2/src/cse360assignment02/AddingMachine.java
+++ Assignment2/src/cse360assignment02/AddingMachine.java
@@ -4,30 +4,38 @@ package cse360assignment02;
  * AddingMachine can add to, subtract from, clear, and return it's total.
  * 
  * @author Jordan Perry
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 public class AddingMachine {
   private int total;
+  private String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total += value;
+	  history += " + " + value;
   }
 
   public void subtract (int value) {
+	  total -= value;
+	  history += " - " + value;
   }
 
   public String toString () {
-    return "";
+    return history;
   }
 
   public void clear() {
+	  total = 0;
+	  history = "0";
   }
 }
